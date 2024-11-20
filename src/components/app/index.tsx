85fe8b3daca869a5976a4054edc0e017bef3749d
nayavii <nayavii17@gmail.com>
2024-11-20 19:27:11 +0100

    updated

--- src/components/app/index.tsx
+++ src/components/app/index.tsx
@@ -31,7 +31,7 @@ export const App:FC = () => {
   }
 
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Header setIsShowLogin={handleChangeShowLogin} />
       <Body
         setIsShowLogin={handleChangeShowLogin}
